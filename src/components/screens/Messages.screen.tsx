6cc353e0d86677ee761fa5e3c50b560c6ea7073f
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-01-16 13:05:19 +0000

    Feat: Add currency listing
    - Add new component called `CurrencyListingView` that displays the names
      of some currencies listed on some free api. The code needs to be
      refactored to make more sense.

--- src/components/screens/Messages.screen.tsx
+++ src/components/screens/Messages.screen.tsx
@@ -2,28 +2,21 @@ import React, { type FC } from "react";
 import { type NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { type AppNavigatorParams } from "_types/navigation.types";
+import CurrencyListView from "_views/currency-list/CurrencyList.view";
 
 import {
   Button,
   Divider,
-  Icon,
-  Layout,
   Text,
   TopNavigation,
   TopNavigationAction,
 } from "@ui-kitten/components";
-import { type RenderProp } from "@ui-kitten/components/devsupport";
-import { type ImageProps } from "react-native-svg";
 import TopLeftLayout from "_layouts/TopLeft.layout";
 
 type MessagesScreenProps = NativeStackScreenProps<
   AppNavigatorParams,
   "Messages"
 >;
-
-const BackIcon: RenderProp<Partial<ImageProps>> = (props) => (
-  <Icon name="facebook" {...props} />
-);
 
 const MessagesScreen: FC<MessagesScreenProps> = ({ navigation }) => {
   const navigateBack = () => {
@@ -43,7 +36,8 @@ const MessagesScreen: FC<MessagesScreenProps> = ({ navigation }) => {
           accessoryLeft={BackAction}
         />
         <Divider />
-        <BackIcon height={20} width={20} />
+        <CurrencyListView />
+        {/* <BackIcon height={20} width={20} /> */}
         <Text category="h1">Messages</Text>
         <Button>Button</Button>
       </TopLeftLayout>
