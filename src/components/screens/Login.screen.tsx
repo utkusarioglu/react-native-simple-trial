09eb6747a3f91e1f81191b01ec79d10a311ed53c
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-01-19 10:04:58 +0000

    Feat: Build a login page
    - Create components `LoginView`, `LoginLayout` and update the component
      `LoginScreen` to create a semi-decent looking login page.

--- src/components/screens/Login.screen.tsx
+++ src/components/screens/Login.screen.tsx
@@ -1,10 +1,9 @@
 import React, { type FC } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Button } from "react-native";
 import { type NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, Button } from "react-native-paper";
 import { type AppNavigatorParams } from "_types/navigation.types";
-import CenterCenterLayout from "_layouts/CenterCenter.layout";
+import LoginLayout from "_layouts/login.layout";
 
 type LoginScreenParams = NativeStackScreenProps<AppNavigatorParams, "Login"> & {
   loginOnPress: () => void;
@@ -15,39 +14,18 @@ const LoginScreen: FC<LoginScreenParams> = ({
   loginOnPress,
 }) => (
   <SafeAreaView style={styles.safeAreaView}>
-    <CenterCenterLayout>
-      <Text>Log in screen</Text>
-      <Text>Will login in 3 seconds</Text>
-      <View style={styles.buttonContainer}>
-        <Button onPress={loginOnPress} style={styles.button}>
-          Login
-        </Button>
-        <Button
-          style={styles.button}
-          onPress={() => navigate("Help", { content: "This is the content" })}>
-          Help
-        </Button>
-      </View>
-    </CenterCenterLayout>
+    <LoginLayout />
+    {/* <Button onPress={loginOnPress} title="Login" />
+    <Button
+      title="Help"
+      onPress={() => navigate("Help", { content: "This is the content" })}
+    /> */}
   </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonContainer: {
-    width: "100%",
-    padding: 16,
-  },
-  button: {
-    marginBottom: 8,
-    width: "100%",
   },
 });
 
